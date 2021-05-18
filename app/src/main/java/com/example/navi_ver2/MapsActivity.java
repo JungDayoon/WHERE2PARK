bfbb4ed3600d83f174ad8d5a43dd0af881c9b05d
Jihae Na <njnjh0317@gmail.com>
2021-05-18 15:18:03 +0900

    check google map api but have to change because of billing

--- app/src/main/java/com/example/navi_ver2/MapsActivity.java
+++ app/src/main/java/com/example/navi_ver2/MapsActivity.java
@@ -283,18 +283,19 @@ public class MapsActivity extends AppCompatActivity
 
         String latitude;
         String longitude;
-        System.out.println(addressList.get(0).toString());
+
         // 콤마를 기준으로 split
         String[] splitStr = addressList.get(0).toString().split(",");
-        String address = splitStr[0].substring(splitStr[0].indexOf("\"") + 1, splitStr[0].length() - 2); // 주소
-        System.out.println(address);
+        String address = splitStr[0].substring(splitStr[0].indexOf("\"") + 1, splitStr[0].length()); // 주소
+        System.out.println("address : " +address);
 
-        latitude = splitStr[10].substring(splitStr[10].indexOf("=") + 1); // 위도
-        longitude = splitStr[12].substring(splitStr[12].indexOf("=") + 1); // 경도
-        System.out.println(latitude);
-        System.out.println(longitude);
+        latitude = splitStr[14].substring(splitStr[14].indexOf("=") + 1); // 위도
+        longitude = splitStr[16].substring(splitStr[16].indexOf("=") + 1); // 경도
+        System.out.println("latitude : "+latitude);
+        System.out.println("longitude : "+longitude);
 
         // 좌표(위도, 경도) 생성
+
         DestPoint = new LatLng(Double.parseDouble(latitude), Double.parseDouble(longitude));
         // 마커 생성
         DestOptions = new MarkerOptions();
